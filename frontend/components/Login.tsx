39430852b8751d3677c1d3fbf935258dbbcc6d9e
Willian Michel <willianmi0017@gmail.com>
2026-08-17 17:14:25 -0300

    configure frontend api url

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -28,7 +28,7 @@ export default function Login({
 
     try {
       const response = await fetch(
-        "http://localhost:8000/auth/login",
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
         {
           method: "POST",
           headers: {
